82cf8e78f13d9f189f52d2c627b2c6188b3eea5a
song <81035270+kimkoko@users.noreply.github.com>
2021-10-31 00:03:15 +0900

    Update mongo-init.js

--- Menu/mongo-init.js
+++ Menu/mongo-init.js
@@ -1,14 +1,9 @@
 db.auth('comp3122', '12345')
-db = db.getSiblingDB('university')
+db = db.getSiblingDB('polyueats')
 
-db.createCollection('student');
+db.createCollection('menu');
 
-db.student.insertOne({'student_id':'33333', 'name':'Alice', 'dept_name':'Comp. Sci.', 'gpa':3.1});
-db.student.insertOne({'student_id':'22222', 'name':'Bob', 'dept_name':'History.', 'gpa':2.0});
-db.student.insertOne({'student_id':'11111', 'name':'Carol', 'dept_name':'History', 'gpa':2.1});
+db.menu.insertOne({'student_id':'33333', 'name':'Alice', 'dept_name':'Comp. Sci.', 'gpa':3.1});
+db.menu.insertOne({'student_id':'22222', 'name':'Bob', 'dept_name':'History.', 'gpa':2.0});
+db.menu.insertOne({'student_id':'11111', 'name':'Carol', 'dept_name':'History', 'gpa':2.1});
 
-
-db.createCollection('takes');
-db.takes.insertOne({'student_id':'33333', 'course_id':'COMP1234', 'credits':1});
-db.takes.insertOne({'student_id':'22222', 'course_id':'COMP1234', 'credits':1});
-db.takes.insertOne({'student_id':'22222', 'course_id':'COMP2345', 'credits':3});
